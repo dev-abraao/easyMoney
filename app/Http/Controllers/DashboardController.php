a6017ca67ccfe31d33ed00c31ed45e7ae296df71
dev-abraao <abraao_Andrade_sousa@hotmail.com>
2025-10-08 12:10:34 -0300

    feat: userexpense list styling

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\ExpenseTypes;
+use App\Models\UserExpense;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
 
-        $userExpenses = auth()->user()->expenses;
+        $userExpenses = UserExpense::where('user_id', auth()->id())
+            ->with('type') // Eager load the related expense type
+            ->orderBy('created_at', 'desc')
+            ->get();
         return view('dashboard', ['expenses' => $userExpenses]);
     }
 }
--- resources/views/components/expense-card.blade.php
+++ resources/views/components/expense-card.blade.php
@@ -1,7 +1,8 @@
 @props(['expense'])
 <div class=" bg-[#00d19f]/10 p-4 border-1 border-gray-300 rounded-lg m-4">
     <p class="font-bold text-lg text-[#00d19f]">{{ $expense->type->name }}</p>
-    <p>{{ $expense->amount }}</p>
+    <p class="font-black text-green-800 truncate">{{ $expense->description }}</p>
+    <p class="font-black text-green-800">{{ $expense->amount }}$</p>
     <p>{{ Carbon\Carbon::parse($expense->date)->format('d/m/Y') }}</p>
     <p class="font-mono text-xs">View details > </p>
 </div>
