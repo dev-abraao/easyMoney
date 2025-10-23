f2758158b14b3ce722086da036a24cec201ba24c
dev-abraao <abraao_Andrade_sousa@hotmail.com>
2025-10-23 15:57:16 -0300

    feat: cron jobs v1

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\RecurringBalance;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// Add Recurring Balances to the account balance
+Schedule::call(function () {
+    DB::transaction(function () {
+        $balances = RecurringBalance::where('next_due_date', '=', now()->toDateString())->get();
+
+        foreach ($balances as $balance) {   
+            $balance->user->balance->increment('balance_amount', $balance->rec_bal_amount);
+
+            $balance->next_due_date = match ($balance->frequency) {
+                'daily' => now()->addDay(),
+                'weekly' => now()->addWeek(),
+                'monthly' => now()->addMonth(),
+                'yearly' => now()->addYear(),
+            };
+
+            $balance->save();
+        }
+    });
+})->daily();
